a8fbc88ec9fce374511d54e50c11fc4b0c4501c1
vineetsinha2016 <vineet.sinha07@gmail.com>
2020-02-21 21:24:49 -0500

    ValidParenthesis

---
+++ src/main/java/com/vin/practise/leetcode/Amazon/easy/Problem4_ValidParentheses.java
@@ -0,0 +1,60 @@
+package com.vin.practise.leetcode.Amazon.easy;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ *
+ * Input: "()"
+ * Output: true
+ *
+ * Input: "()[]{}"
+ * Output: true
+ *
+ * Input: "(]"
+ * Output: false
+ *
+ * Input: "([)]"
+ * Output: false
+ *
+ * Input: "{[]}"
+ * Output: true
+ */
+public class Problem4_ValidParentheses {
+     static boolean isValid(String s) {
+         if (s!=null) {
+             if (s.equals(" ") || s.equals("")) return true;
+             HashMap<Character, Character> hashMap = new HashMap<>();
+             hashMap.put('(',')');hashMap.put('{','}');hashMap.put('[',']');
+             Stack<Character> stack = new Stack<>();
+             char[] characters = s.toCharArray();
+             for (int i =0;i<s.length();i++) {
+                 if (hashMap.containsKey(characters[i])) {
+                     stack.push(characters[i]);
+                 }
+                 else {
+                     if (stack.empty()) return false;
+                     if (!hashMap.get(stack.pop()).equals(characters[i])) {
+                         return false;
+                     }
+                 }
+             }
+             if (!stack.empty()) {
+                 System.out.println("something left behind");
+                    return false;
+             }
+         }
+        return true;
+    }
+    public static void main(String [] args) {
+        String str = "]";
+        System.out.println("Result -->"+isValid(str));
+    }
+}
