340b6efe5682c07ad6d9aeed77ac114b4b454929
vineetsinha2016 <vineet.sinha07@gmail.com>
2020-02-24 21:26:03 -0500

    Minimum Cost of ropes

---
+++ src/main/java/com/vin/practise/leetcode/Amazon/oa/MinCostToConnectRopes.java
@@ -0,0 +1,62 @@
+package com.vin.practise.leetcode.Amazon.oa;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * Given n ropes of different lengths, we need to connect these ropes into one rope. We can connect only 2 ropes at a time.
+ * The cost required to connect 2 ropes is equal to sum of their lengths. The length of this connected rope is
+ * also equal to the sum of their lengths. This process is repeated until n ropes are connected into a single rope.
+ * Find the min possible cost required to connect all ropes.
+ *
+ * Example 1:
+ *
+ * Input: ropes = [8, 4, 6, 12]
+ * Output: 58
+ * Explanation: The optimal way to connect ropes is as follows
+ * 1. Connect the ropes of length 4 and 6 (cost is 10). Ropes after connecting: [8, 10, 12]
+ * 2. Connect the ropes of length 8 and 10 (cost is 18). Ropes after connecting: [18, 12]
+ * 3. Connect the ropes of length 18 and 12 (cost is 30).
+ * Total cost to connect the ropes is 10 + 18 + 30 = 58
+ *
+ * Example 2:
+ *
+ * Input: ropes = [20, 4, 8, 2]
+ * Output: 54
+ *
+ * Example 3:
+ *
+ * Input: ropes = [1, 2, 5, 10, 35, 89]
+ * Output: 224
+ *
+ * Example 4:
+ *
+ * Input: ropes = [2, 2, 3, 3]
+ * Output: 20
+ *
+ * Step 1: Initialize totalCost to 0 and run while loop on queue if size is more than one. keep looping
+ * Step 2: poll two elements , add them and put into a temp variable
+ * Step 3: add this temp to existing queue and add it to totalCost.
+ * Step 4: return totalCost.
+ */
+public class MinCostToConnectRopes {
+    public static void main (String[] args) {
+        Integer[] ropes ={2, 2, 3, 3};
+        System.out.println("Result-->"+minPossibleCost(ropes));
+    }
+    static Integer minPossibleCost(Integer[] ropes) {
+        List<Integer> ropesList = new ArrayList<>();
+        Collections.addAll(ropesList,ropes);
+        PriorityQueue<Integer> queue = new PriorityQueue<>(ropesList);
+        int total =0;
+        while (queue.size()>1) {
+            int temp = queue.poll() + queue.poll();
+            queue.add(temp);
+            total = total + temp;
+        }
+        return total;
+
+    }
+}
