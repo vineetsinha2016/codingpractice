191ed61a0dd7a18136fef8065c0751581760b6a3
vineetsinha2016 <vineet.sinha07@gmail.com>
2020-05-17 18:30:43 -0400

    Copied solution

--- src/main/java/com/vin/practise/leetcode/general/design/DesignHashSetUsingBST.java
+++ src/main/java/com/vin/practise/leetcode/general/design/DesignHashSetUsingBST.java
@@ -5,10 +5,10 @@ package com.vin.practise.leetcode.general.design;
  *
  */
 public class DesignHashSetUsingBST {
-    private Bucket[] buckets;
-    private Integer keyRange;
+    /*private Bucket[] buckets;
+    private Integer keyRange;*/
     /** Initialize your data structure here. */
-    public DesignHashSetUsingBST() {
+    /*public DesignHashSetUsingBST() {
         this.keyRange=769;
         this.buckets = new Bucket[this.keyRange];
         for (int i=0;i<keyRange;i++) {
@@ -27,15 +27,15 @@ public class DesignHashSetUsingBST {
     public void remove(int key) {
         int index = this._hash(key);
         buckets[index].delete(key);
-    }
+    }*/
 
     /** Returns true if this set contains the specified element */
-    public boolean contains(int key) {
+    /*public boolean contains(int key) {
         int index = this._hash(key);
         return buckets[index].exists(key);
-    }
+    }*/
 
-    class Bucket {
+   /* class Bucket {
         private MyBST myBST;
         public Bucket() {
             myBST = new MyBST();
@@ -58,13 +58,13 @@ public class DesignHashSetUsingBST {
         public TreeNode(int val) {
             this.val = val;
         }
-    }
-    class MyBST {
-        private TreeNode root = null;
+    }*/
+    //class MyBST {
+        //private TreeNode root = null;
         /*public MyBST(int val) {//TODO :Learn the lesson . Not required
             this.root = new TreeNode(val);
         }*/
-        public TreeNode searchMyBST (TreeNode root, int val) {
+        /*public TreeNode searchMyBST (TreeNode root, int val) {
             if (root==null || val == root.val) return root;
             return (val<root.val)  ? searchMyBST(root.left, val)
             :  searchMyBST(root.right, val);
@@ -79,13 +79,13 @@ public class DesignHashSetUsingBST {
                 root.left = insertIntoMyBST(root.left, val);
             }
             return root;
-        }
+        }*/
         /**
          * to support delete node from BST we need following 2 methods :
          * findPredecessor and findSuccessor
          */
         //Formula ; One step left and then always right
-        public int findPredecessor(TreeNode root) {
+        /*public int findPredecessor(TreeNode root) {
             root = root.left;
             while (root.right!=null) root = root.right;
             return root.val;
@@ -117,6 +117,145 @@ public class DesignHashSetUsingBST {
                 }
             }
             return root;
+        }*/
+   // }
+
+        private Bucket[] bucketArray;
+    private int keyRange;
+
+    /** Initialize your data structure here. */
+    public DesignHashSetUsingBST() {
+        this.keyRange = 769;
+        this.bucketArray = new Bucket[this.keyRange];
+        for (int i = 0; i < this.keyRange; ++i)
+            this.bucketArray[i] = new Bucket();
+    }
+
+    protected int _hash(int key) {
+        return (key % this.keyRange);
+    }
+
+    public void add(int key) {
+        int bucketIndex = this._hash(key);
+        this.bucketArray[bucketIndex].insert(key);
+    }
+
+    public void remove(int key) {
+        int bucketIndex = this._hash(key);
+        this.bucketArray[bucketIndex].delete(key);
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        int bucketIndex = this._hash(key);
+        return this.bucketArray[bucketIndex].exists(key);
+    }
+}
+
+
+class Bucket {
+    private BSTree tree;
+
+    public Bucket() {
+        tree = new BSTree();
+    }
+
+    public void insert(Integer key) {
+        this.tree.root = this.tree.insertIntoBST(this.tree.root, key);
+    }
+
+    public void delete(Integer key) {
+        this.tree.root = this.tree.deleteNode(this.tree.root, key);
+    }
+
+    public boolean exists(Integer key) {
+        TreeNode node = this.tree.searchBST(this.tree.root, key);
+        return (node != null);
+    }
+}
+
+ class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class BSTree {
+    TreeNode root = null;
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || val == root.val)
+            return root;
+
+        return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null)
+            return new TreeNode(val);
+
+        if (val > root.val)
+            // insert into the right subtree
+            root.right = insertIntoBST(root.right, val);
+        else if (val == root.val)
+            // skip the insertion
+            return root;
+        else
+            // insert into the left subtree
+            root.left = insertIntoBST(root.left, val);
+        return root;
+    }
+
+    /*
+     * One step right and then always left
+     */
+    public int successor(TreeNode root) {
+        root = root.right;
+        while (root.left != null)
+            root = root.left;
+        return root.val;
+    }
+
+    /*
+     * One step left and then always right
+     */
+    public int predecessor(TreeNode root) {
+        root = root.left;
+        while (root.right != null)
+            root = root.right;
+        return root.val;
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null)
+            return null;
+
+        // delete from the right subtree
+        if (key > root.val)
+            root.right = deleteNode(root.right, key);
+            // delete from the left subtree
+        else if (key < root.val)
+            root.left = deleteNode(root.left, key);
+            // delete the current node
+        else {
+            // the node is a leaf
+            if (root.left == null && root.right == null)
+                root = null;
+                // the node is not a leaf and has a right child
+            else if (root.right != null) {
+                root.val = successor(root);
+                root.right = deleteNode(root.right, root.val);
+            }
+            // the node is not a leaf, has no right child, and has a left child
+            else {
+                root.val = predecessor(root);
+                root.left = deleteNode(root.left, root.val);
+            }
         }
+        return root;
     }
 }
